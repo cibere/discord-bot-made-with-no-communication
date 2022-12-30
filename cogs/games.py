67ffdcb38ad7a2e0c7a59d17b773aed594f87988
HumbleToS <ileakpics2020@gmail.com>
2022-12-29 18:35:29 -0700

    Fix: not using type_checking for bot import from main

--- cogs/games.py
+++ cogs/games.py
@@ -1,11 +1,17 @@
-import discord
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from discord.ext import commands
 
-from main import Bot
+if TYPE_CHECKING:
+    from main import Bot
 
-async def setup(bot: Bot):
-    await bot.add_cog(Games(bot))
 
 class Games(commands.Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
+
+
+async def setup(bot: Bot):
+    await bot.add_cog(Games(bot))
